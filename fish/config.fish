b3d425e1b82202737684ea1d7534f883fc6e5ffd
Sharad Ganapathy <sharadgana@gmail.com>
2018-02-07 22:35:56 -0800

    Add bobthefish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,14 @@
 set -xU GOPATH /Users/sharadg/workspace/go
 set -xU PATH $PATH /Users/sharadg/workspace/go/bin
 alias vim nvim
+
+#bobthefish
+
+set -g theme_color_scheme solarized
+
+function fish_greeting
+  set_color $fish_color_autosuggestion
+  echo "I'm completely operational, and all my circuits are functioning perfectly."
+  set_color normal
+end
+
